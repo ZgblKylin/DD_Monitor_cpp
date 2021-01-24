4ea2e719c7defda2b98f376f3a3844e58f5bb64d
ZgblKylin <fei.chong@foxmail.com>
2021-01-25 00:29:46 +0800

    GitHub Workflow for Windows

--- scripts/build_macos.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/zsh
-pyinstaller DD监控室.spec
-cp -r utils/ dist/DD监控室/utils/
---
+++ src/common.h
@@ -0,0 +1,10 @@
+#pragma once
+#ifndef DDMONITOR_COMMON_HPP
+#define DDMONITOR_COMMON_HPP
+
+#include <QtCore/QtCore>
+#include <QtNetwork/QtNetwork>
+#include <QtWidgets/QtWidgets>
+#include <QtNetwork/QtNetwork>
+
+#endif  // DDMONITOR_COMMON_HPP
